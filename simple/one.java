316376ab52565f089be87a833d39410a19dd7b99
mohityeole <mohit.yeole@kapittx.com>
2025-04-10 23:39:31 +0530

    second commit

--- simple/one.java
+++ simple/one.java
@@ -5,6 +5,7 @@ public class one {
 	public static void main(String[] args) {
 		
 		System.out.println("hello world");
+		System.out.println("hello java");
 
 	}
 
---
+++ simple/two.java
@@ -0,0 +1,10 @@
+package simple;
+
+public class two {
+
+	public static void main(String[] args) {
+		System.out.println("second");
+
+	}
+
+}
